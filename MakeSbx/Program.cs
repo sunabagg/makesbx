9a8af74e97535078dbc6078e475840a2fbecf1e9
Lucas C. Abbas <lucasbear9@gmail.com>
2025-04-03 10:42:48 -0800

    refactor Program.cs to define a Program class and encapsulate the Main method

--- MakeSbx/Program.cs
+++ MakeSbx/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace MakeSbx;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
